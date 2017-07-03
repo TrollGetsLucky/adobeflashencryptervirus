9df11425fc8599fc006257e80a97f7bb3a981a78
mwear3robloxandroid <mitchellwears@gmail.com>
2017-07-03 03:14:47 -0400

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,1 +1,12 @@
 <link rel="stylesheet" href="videojs.ads.css">
+  
+<video id="video" src="movie.mp4" controls></video>
+<script src="video.js"></script>
+<script src="videojs.ads.js"></script>
+<script>
+videojs('video', {}, function() {
+  var player = this;
+  player.ads(); // initialize the ad framework
+  // your custom ad integration code
+});
+</script>
